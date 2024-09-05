868cda3ae0dc363f563aeff393ea37bf68757cfa
Rahul Pandey <0517rp.rahul@gmail.com>
2024-09-05 21:54:07 +0530

    comments and variables

---
+++ 02.program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // variables
+
+    int n = 10;
+    float f = 8.0;
+    char ch = 'R';
+    string str = "Rahul";
+
+    cout << n << endl;
+    cout << f << endl;
+    cout << ch << endl;
+    cout << str << endl;
+    
+    return 0;
+
+}
