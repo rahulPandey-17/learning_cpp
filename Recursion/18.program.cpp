a772bc6cbd43729926d3d8d1d4cc3b7cba3a5f1d
Rahul Pandey <0517rp.rahul@gmail.com>
2024-09-08 16:22:45 +0530

    recursion

--- Recursion/18.program.cpp
+++ Recursion/18.program.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//recursion
+//recursion for factorial
 
 int facto(int num) {
 
@@ -14,6 +14,22 @@ int facto(int num) {
 
 }
 
+// recursion for fibonacci series
+
+int fibo(int n) {
+
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n == 1) {
+        return 1;
+    }
+
+    return fibo(n - 1) + fibo(n - 2);
+
+}
+
 int main() {
 
     int num;
@@ -22,6 +38,16 @@ int main() {
     cin >> num;
 
     cout << "The factorial of " << num << " is " << facto(num) << endl;
+
+    int n;
+
+    cout << "Enter the no. of digits of fibonacci you want to see : ";
+    cin >> n;
+
+    for (int i = 0 ; i < n ; i++) {
+        cout << fibo(i) << " ";
+    }
+    cout << endl;
 
     return 0;
 
